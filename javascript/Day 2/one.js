0cc2b0290cac8d424b1330e98dfe32d92fa09182
PramendraShukla1 <shuklapramendra99@gmail.com>
2025-04-17 09:33:27 +0530

    introduction to javascript, declaration of variable, data types in javascript, scope of variable, Arithmetic operator, Assignment operators

---
+++ javascript/Day 2/one.js
@@ -0,0 +1,93 @@
+//Single line comment
+//console.log("Hello world");
+
+//Multi-line comment
+/*
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+*/
+
+//Declaration of variable (let, const, var)
+// var a = 10;
+// var a = 30;
+// console.log(a);
+
+// const a = 10;
+// const a = 30;
+
+// let a = 10;
+// let a = 20;
+// console.log(a);
+
+//Data Types in Javascript
+// Primitive Data Types
+// let n = 10;
+// console.log(typeof n);
+// let n1 = "String";
+// console.log(typeof n1);
+// let n2 = false;
+// console.log(typeof n2);
+// let n3;
+// console.log(typeof n3);
+// let n4 = null;
+// console.log(typeof n4);
+// let n5 = Symbol("Hello");
+// console.log(typeof n5);
+// let n6 = 123456789012345678901234567890n;
+// console.log(typeof n6);
+//Non Primitive Data Types
+// let obj = {
+//   name: "test user",
+//   age: 25,
+// };
+// console.log(typeof obj);
+
+// let n7 = [1, 2, 3, 4, 5];
+// console.log(typeof n7);
+
+// function sayHello() {
+//   console.log("Good Evening!");
+// }
+// sayHello();
+
+// console.log(typeof sayHello);
+
+// Global and Local Variables in Javascript
+
+// let a = 20;
+
+// function sayHello() {
+//   var time = "Evening!";
+//   console.log(time + "___" + a);
+// }
+// var time = "Morning!";
+
+// sayHello();
+// console.log(time);
+
+// Javascript Operators
+//Arithmetic Operators
+
+let a = 10;
+let b = "hello";
+let sum = a - b;
+// let sub = a - b;
+// let mul = a * b;
+// let div = a / b;
+// console.log(sum);
+// console.log(sub);
+// console.log(mul);
+// console.log(div);
+//console.log(sum);
+//Assignment Operators
+// let n = 10;
+// console.log(n);
+// n += 5;
+// console.log(n);
+// n *= 5;
+// console.log(n);
+// n -= 5;
+// console.log(n);
+// n /= 5;
+// console.log(n);

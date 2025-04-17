651f2bfdb91d6aced8f0ec22918338c8c97b22e8
PramendraShukla1 <shuklapramendra99@gmail.com>
2025-04-17 22:05:19 +0530

    Operators, switch, break, continue and conditional statement

--- javascript/Day 2/one.js
+++ javascript/Day 2/one.js
@@ -67,11 +67,12 @@ console.log("Hello world");
 // console.log(time);
 
 // Javascript Operators
+
 //Arithmetic Operators
 
-let a = 10;
-let b = "hello";
-let sum = a - b;
+// let a = 10;
+// let b = "hello";
+// let sum = a - b;
 // let sub = a - b;
 // let mul = a * b;
 // let div = a / b;
@@ -80,7 +81,9 @@ let sum = a - b;
 // console.log(mul);
 // console.log(div);
 //console.log(sum);
+
 //Assignment Operators
+
 // let n = 10;
 // console.log(n);
 // n += 5;
@@ -91,3 +94,128 @@ let sum = a - b;
 // console.log(n);
 // n /= 5;
 // console.log(n);
+//console.log("hello world");
+
+//Comparison Operators
+
+// let a = 20;
+// let b = 40;
+// console.log(a >= b);
+
+// Javascript Logical Operators
+
+// let a = false;
+// let b = false;
+// console.log(a || b);
+
+// Javascript Ternary Operator
+
+// const a = 18;
+// const status = a >= 18 ? "You can vote" : "You cannot vote";
+// console.log(status);
+
+// Javascript Comma Operator
+
+// let n1, n2;
+// const res = ((n1 = 10), (n2 = 10), n1 + n2);
+// console.log(res);
+
+// Unary Operators
+
+// let a = 5;
+// console.log(--a);
+// console.log(++a);
+
+// BigInt Operator
+
+// let n = 123456789012345678901234567890n;
+// let n1 = 123456789012345678901234567890n;
+// console.log(n + n1);
+
+// let a = "Hello";
+// let c = "World";
+// console.log(a + " " + c);
+
+// let a = 16;
+
+// if (a == 16) {
+//   if (a < 15) {
+//     console.log("a is less than 15");
+//   } else {
+//     console.log("a is greater than 15");
+//   }
+// } else {
+//   console.log("a is not equal to 10");
+// }
+
+// let score = 60;
+// if (score >= 90 && score <= 100) {
+//   console.log("A");
+// } else if (score >= 80 && score < 90) {
+//   console.log("B");
+// } else if (score >= 70 && score < 80) {
+//   console.log("C");
+// } else {
+//   console.log("You are fail!");
+// }
+
+// Switch Case Statement
+// const day = 8;
+// let dayName;
+
+// switch (day) {
+//   case 1:
+//     dayName = "Monday";
+//     break;
+//   case 2:
+//     dayName = "Tuesday";
+//     break;
+//   case 3:
+//     dayName = "Wednesday";
+//     break;
+//   case 4:
+//     dayName = "Thursday";
+//     break;
+//   case 5:
+//     dayName = "Friday";
+//     break;
+//   case 6:
+//     dayName = "Saturday";
+//     break;
+//   case 7:
+//     dayName = "Sunday";
+//     break;
+//   default:
+//     dayName = "Invalid day";
+//     break;
+// }
+
+// console.log(dayName);
+
+// let grade = "A";
+// let result;
+
+// switch (grade) {
+//   case "A":
+//     result = "Excellent";
+//     break;
+//   case "B":
+//     result = "Good";
+//     break;
+//   case "C":
+//     result = "Average";
+//     break;
+//   case "D":
+//     result = "Poor";
+//     break;
+//   default:
+//     result = "Invalid grade";
+//     break;
+// }
+
+// console.log(result);
+
+for (let i = 1; i <= 10; i++) {
+  if (i == 4) break;
+  console.log(i);
+}
